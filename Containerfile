4a8c7c20a3e7d839d99b0eeb2ef40f000d8c5c97
Glenn Brown <glenn.brown@gmail.com>
2025-05-29 23:53:04 -0400

    Change:  - Switched to stable-zfs tag for upstream ucore-hci  - Add a bunch of packages and fixed homebrew dependencies

--- Containerfile
+++ Containerfile
@@ -3,7 +3,7 @@ FROM scratch AS ctx
 COPY build_files /
 
 # Base Image
-FROM ghcr.io/ublue-os/ucore-hci:stable-nvidia-zfs
+FROM ghcr.io/ublue-os/ucore-hci:stable-zfs
 
 ## Other possible base images include:
 # FROM ghcr.io/ublue-os/bazzite:latest
--- build_files/build.sh
+++ build_files/build.sh
@@ -9,11 +9,30 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
+# This installs a package from fedora repos
+# Homebrew Dependencies
+dnf group install -y 'Development Tools'
+# Homebrew via ublue-os copr repo
 dnf copr enable -y ublue-os/packages
 dnf install -y ublue-brew
 dnf copr disable -y ublue-os/packages
-
+#### Many of these packages do exist in brew but I think they make sense to install from Fedora
+# lm_sensors, smartmontools
+dnf install -y lm_sensors smartmontools
+# Note: git-core is installed by default just adding more tools
+dnf install -y git git-crypt
+# Legacy tools, probaly don't need thme but nice to have
+dnf install -y ftp telnet rsh whois tftp
+# Compression tools
+dnf install -y p7zip zip unzip
+# Disk tools (snapper is for being able to do snapraid-btrfs)
+dnf install -y bcachefs-tools sg3_utils hddtemp snapper nvme-cli
+# Intel GPU Tools and Drivers 
+dnf install -y igt-gpu-tools intel-media-driver-free libvdpau-va-gl intel-compute-runtime intel-gmmlib libva libva-utils glx-utils
+# Extra tools 
+dnf install -y busybox ipmitool just lsb_release redhat-lsb iotop-c pipx strace
+# zsh
+dnf install -y zsh
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
